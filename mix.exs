3c150e5273fd744b913fe2b3da80e0fe9d2263bb
Gage Peterson <justgage@gmail.com>
2020-04-25 07:25:06 -0600

    add a few deps that will make things easier

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule PizzaParty.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
+      {:mix_test_watch, "~> 1.0.2", only: :dev, runtime: false}
+
     ]
   end
 
